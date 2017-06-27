7e5adf2ad2856db24fd95c5fe223cd4eba3936a6
daderoxas <danthony0209@gmail.com>
2017-06-28 00:54:38 +0800

    Add files via upload

---
+++ mem_check.pp
@@ -0,0 +1,61 @@
+class puppet{
+	package{ 'vim': 
+		ensure => 'installed',
+	}
+	
+	package{ 'curl':
+		ensure => 'installed',
+	}
+
+	package{ 'git' :
+		ensure => 'installed',
+	}
+
+
+	user { 'monitor':	
+		ensure => 'present',
+		home => '/home/monitor',
+		managehome => true,
+		shell => '/bin/bash',
+	}
+
+
+	file { '/home/monitor/scripts': 
+		ensure => 'directory',
+	}
+
+	exec { 'wget':
+		command => '/usr/bin/wget https://rawgit.com/daderoxas/SE_exercise/master/memory_check.sh -O memory_check', 
+		path => '/home/monitor/scripts',
+		cwd => '/home/monitor/scripts',
+	}	
+
+
+	file { '/home/monitor/src/': 
+		ensure => 'directory',
+	}
+	
+	file { '/home/monitor/src/my_memory_check':
+		ensure => '/home/monitor/scripts/memory_check',
+	}
+
+
+	cron { 'memcheck': 
+		command => 'sh /home/monitor/src/my_memory_check -w 60 -c 90 -e danthony0209@gmail.com',
+		user => 'root',
+		minute => '*/10',
+	}
+
+
+	class {
+		timezone => 'PHT',
+	}
+
+	host { 
+  		name => 'bpx.server.local',
+	}
+	
+	
+}
+
+include puppet
